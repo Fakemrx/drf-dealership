1e2c6d1a8622919bc89ce2a10f43ad5efca87bae
Василий <kochanvasia@mail.ru>
2023-02-14 11:45:55 +0300

    Added .gitignore

--- tests/initial_responce_test.py
+++ tests/initial_responce_test.py
@@ -1,13 +1,12 @@
 from django.test import Client
-from django.conf import settings
-
-if settings.DEBUG:
-    def check_acceptable_response():
-        c = Client()
-        response = c.get('')
-        print(response.status_code)
-        return response.status_code
 
 
-    assert check_acceptable_response() == 200
-    print('Tests are done!')
+def check_acceptable_response():
+    c = Client()
+    response = c.get('')
+    print(response.status_code)
+    return response.status_code
+
+
+assert check_acceptable_response() == 200
+print('Tests are done!')
