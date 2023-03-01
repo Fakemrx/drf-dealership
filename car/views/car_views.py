0295ca4a3cc56bb9ddb204e6e7f7104929636747
Василий <kochanvasia@mail.ru>
2023-03-01 19:19:51 +0300

    Full fix: 1) Removed filtering/ordering/sorting tests; 2) Edited service for postgres in docker-compose.yml; 3) Added correct CRUD tests; 4) Remade APIViews to mixins instead of generics.

--- car/views/car_views.py
+++ car/views/car_views.py
@@ -1,13 +1,21 @@
 """Module of CRUD APIViews for Car model."""
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, filters
+from rest_framework import filters
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+    RetrieveModelMixin,
+)
 
 from car.filters import CarFilter
 from car.models import Car
 from car.serializers.car_serializers import CarSerializer
 
 
-class CarListCreateView(generics.ListCreateAPIView):
+class CarListCreateView(GenericAPIView, ListModelMixin, CreateModelMixin):
     """APIView create and list-view operations for Car model."""
 
     queryset = Car.objects.all()
@@ -22,9 +30,35 @@ class CarListCreateView(generics.ListCreateAPIView):
     ordering = ["car_brand"]
     filterset_class = CarFilter
 
+    def post(self, request, *args, **kwargs):
+        """POST method to add car instance."""
+        return self.create(request, *args, **kwargs)
 
-class CarRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    def get(self, request, *args, **kwargs):
+        """GET method to get list of car instances."""
+        return self.list(request, *args, **kwargs)
+
+
+class CarRetrieveUpdateDeleteView(
+    GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+):
     """APIView retrieve, update, delete operations for Car model."""
 
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+    def get(self, request, *args, **kwargs):
+        """GET method to detail car instance."""
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        """PUT method to update car instance."""
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """PATCH method to partial update car instance."""
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        """DELETE method to delete car instance."""
+        return self.destroy(request, *args, **kwargs)
--- car/views/engine_views.py
+++ car/views/engine_views.py
@@ -1,5 +1,13 @@
 """Module of CRUD APIViews for Engine model."""
-from rest_framework import generics, filters
+from rest_framework import filters
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+    RetrieveModelMixin,
+)
 from django_filters.rest_framework import DjangoFilterBackend
 
 from car.filters import EngineFilter
@@ -7,8 +15,8 @@ from car.models import Engine
 from car.serializers.engine_serializers import EngineSerializer
 
 
-class EngineListCreateView(generics.ListCreateAPIView):
-    """APIView create and list-view operations for Car model."""
+class EngineListCreateView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    """APIView create and list-view operations for Engine model."""
 
     queryset = Engine.objects.all()
     serializer_class = EngineSerializer
@@ -22,9 +30,35 @@ class EngineListCreateView(generics.ListCreateAPIView):
     ordering = ["engine_brand"]
     filterset_class = EngineFilter
 
+    def post(self, request, *args, **kwargs):
+        """POST method to add engine instance."""
+        return self.create(request, *args, **kwargs)
 
-class EngineRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    """APIView retrieve, update, delete operations for Car model."""
+    def get(self, request, *args, **kwargs):
+        """GET method to get list of engine instances."""
+        return self.list(request, *args, **kwargs)
+
+
+class EngineRetrieveUpdateDeleteView(
+    GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+):
+    """APIView retrieve, update, partial update, delete operations for Engine model."""
 
     queryset = Engine.objects.all()
     serializer_class = EngineSerializer
+
+    def get(self, request, *args, **kwargs):
+        """GET method to detail engine instance."""
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        """PUT method to update engine instance."""
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """PATCH method to partial update engine instance."""
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        """DELETE method to delete engine instance."""
+        return self.destroy(request, *args, **kwargs)
