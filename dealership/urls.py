109d9770bae7530203c9f82c37294eaa05ce0d2e
Fakemrx <vkachan2003@gmail.com>
2023-04-18 14:30:04 +0300

    APIView for cars in dealership stock

--- dealership/urls.py
+++ dealership/urls.py
@@ -3,9 +3,13 @@ Urls module for Dealership app.
 """
 from rest_framework.routers import SimpleRouter
 
-from dealership.views.car_dealership_views import CarDealershipAPIView
+from dealership.views.car_dealership_views import (
+    CarDealershipAPIView,
+    CarsInStockAPIView,
+)
 
 router = SimpleRouter()
 router.register("dealerships", CarDealershipAPIView)
+router.register("cars-in-stock", CarsInStockAPIView)
 
 urlpatterns = router.urls
